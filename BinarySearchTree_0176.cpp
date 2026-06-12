ea7fcd898a755c1995c1f205ee69ca969a61dbdb
Minion Ngawi <achmadrianta4@gmail.com>
2026-06-12 08:08:19 +0000

    commit first

---
+++ BinarySearchTree_0176.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class node {
+
+
+public:
+    string info;
+    Node* leftChild;
+    Node* RightChild;
+
+    Node(string i, Node* l, Node* r) {
+        info = i;
+        leftChild = l;
+        rightChil
+    }
+}
